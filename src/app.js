2c09e199841fa8c274d349e748018b5427e78cd5
Marius <94065662+MariusHor@users.noreply.github.com>
2022-08-15 02:07:14 +0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,15 @@
+//Scss
+import './scss/main.scss';
+//JavaScript
+import './js/index.js';
+
+import './img/favicon-32x32.png'
+import './img/logo.svg'
+import './img/mobile/image-hero.jpg'
+import './img/icon-hamburger.svg'
+import './img/mobile/image-interactive.jpg'
+import './img/icon-facebook.svg'
+import './img/icon-twitter.svg'
+import './img/icon-pinterest.svg'
+import './img/icon-instagram.svg'
+import './img/icon-close.svg'
