4aca73b22da9bec1874da2ee0abed9c515032d1e
marius <marius.horghidan@yahoo.com>
2022-08-15 19:52:15 +0300

    I finished the desktop layout

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,18 +1,11 @@
 const navMenu = document.querySelector('.fullscreen-menu');
 const hamburger = document.querySelector('.hamburger');
-const closeBtn = document.querySelector('.close-btn');
 const body = document.querySelector('body')
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('active');
     navMenu.classList.toggle('active');
     body.classList.toggle('overflow-hidden')
-});
-
-closeBtn.addEventListener('click', () => {
-    hamburger.classList.remove('active');
-    navMenu.classList.remove('active');
-    body.classList.remove('overflow-hidden')
 });
 
 
